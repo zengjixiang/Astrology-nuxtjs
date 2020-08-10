c013e7d626e142a452b7a4984892b4da7373c6de
zengjixiang <zengjixiang0401@gmail.com>
2020-08-10 13:04:24 +0800

    edit index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <title>Universal Astrology 占星網</title>
-  <div class="links">
-        <a
-          href="./house.vue"
-          target="_blank"
-          class="button--green"
-        >
-          行星座落在十二宮位的影響
-        </a>
-  <option><a href=./house.vue>十二宮位</a></option>
+  <main>
+    <h1></h1>Universal Astrology 占星網</h1>
+    <NuxtLink to="/about">關於</NuxtLink>
+    <a href="/house">行星座落在十二宮位的影響</a>
+    
+     <option><a href=./house.vue>十二宮位</a></option>
+    
+  </main>
+
 </template>
 
 
